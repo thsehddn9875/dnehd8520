0a2d8f3f4ab1c0da6e5b7e0c6d6b42d53f686785
thsehddn9875 <thsehddn9875@naver.com>
2025-03-21 12:07:33 +0900

    git config user.name (github thsehddn9875) >> git config user.email (github thsehddn9875@naver.com)

---
+++ 과제_1.py
@@ -0,0 +1,3 @@
+inch = float(input("인치 값을 입력하세요: "))
+cm = inch * 2.54
+print(inch, "인치는", cm, "센티미터입니다.")
---
+++ 과제_1_2.py
@@ -0,0 +1,3 @@
+kg = float(input("kg 값을 입력하세요: "))
+lb = kg * 2.2046
+print(kg, "kg는", lb, "파운드 입니다.")
